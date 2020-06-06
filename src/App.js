aeef040a433e5c899ba0df34780cb079d9db5a41
vcnsiqueira <35041704+vcnsiqueira@users.noreply.github.com>
2020-06-06 00:41:41 -0300

    First commit of the personal finance project. The idea of this project is to develop an app where people can register every single expense they have by category, selecting the date and value

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      elementos: [
+        {
+          categoria: 'Supermercado',
+          data: '2020-06-05',
+          valor: '100.00'
+        },
+        {
+          categoria: 'Alimentação',
+          data: '2020-06-05',
+          valor: '20.00'
+        },
+        {
+          categoria: 'Farmácia',
+          data: '2020-06-06',
+          valor: '45.00'
+        }
+      ],
+      titulo: ['Categoria', 'Data', 'Valor'],
+    }
+  }
+
+  render() {  
+    return (
+      <Fragment>
+        <h1>Cadastro de despesa</h1>
+        <form>
+          <label for="categoria">Categoria:</label>
+          <input type="text" id="category" name="category" value="Supermercado"/>
+          <label for="data">Data:</label>
+          <input type="date" id="date" name="date"/>
+          <label for="valor">Categoria:</label>
+          <input type="number" id="valor" name="valor" value="100.00"/>
+          <button type="submit">Enviar</button>
+        </form>
+        <hr></hr>
+        <h1>Tabela</h1>
+        <table>
+          <thead>
+            <tr>
+              {
+                this.state.titulo.map(item => {
+                  return(
+                    <th>{item}</th>
+                  );
+                })
+              }
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.elementos.map(elemento => {
+                return(
+                  <tr>
+                    <td>{elemento.categoria}</td>
+                    <td>{elemento.data}</td>
+                    <td>{elemento.valor}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>      
+      </Fragment>
+    );
+  }
 }
 
 export default App;
